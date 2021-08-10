9ac1b2a8c37b30fb976675bd63e83e174a24d382
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-08-10 16:08:05 +0200

    added a path to all methods

--- src/services/authors/index.js
+++ src/services/authors/index.js
@@ -1,11 +1,12 @@
 import express from 'express'
+import { fileURLToPath } from 'node:url'
 
 const authorsRouter = express.Router()
-
+const currentFilePath = fileURLToPath(import.meta.url)
 authorsRouter.post('/',(req,resp)=>{})
-authorsRouter.get('/',(req,resp)=>{resp.send('hello tomish meksen')})
+authorsRouter.get('/',(req,resp)=>{resp.send(currentFilePath)})
 authorsRouter.get('/:id',(req,resp)=>{})
-authorsRouter.put('',(req,resp)=>{})
-authorsRouter.delete('',(req,resp)=>{})
+authorsRouter.put('/:id',(req,resp)=>{})
+authorsRouter.delete('/:id',(req,resp)=>{})
 
 export default authorsRouter

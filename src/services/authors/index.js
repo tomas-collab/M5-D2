1699b32affee4b3fc86af9910efd00a8c7d7b0e3
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-08-10 20:54:33 +0200

    other methods in progress

--- src/services/authors/index.js
+++ src/services/authors/index.js
@@ -1,12 +1,34 @@
 import express from 'express'
-import { fileURLToPath } from 'node:url'
+import { fileURLToPath } from 'url'
+import {dirname,join} from 'path'
+import fs from 'fs'
+
+
 
 const authorsRouter = express.Router()
 const currentFilePath = fileURLToPath(import.meta.url)
-authorsRouter.post('/',(req,resp)=>{})
-authorsRouter.get('/',(req,resp)=>{resp.send(currentFilePath)})
-authorsRouter.get('/:id',(req,resp)=>{})
-authorsRouter.put('/:id',(req,resp)=>{})
-authorsRouter.delete('/:id',(req,resp)=>{})
+const currnetDirPath = dirname(currentFilePath)
+const authorsJSONpath = join(currnetDirPath , 'authors.json')
+
+
+authorsRouter.post('/',(req,resp)=>{resp.send('hello post')})
+authorsRouter.get('/',(request,response)=>{
+    const fileContent = fs.readFileSync(authorsJSONpath)
+   
+    response.send(JSON.parse(fileContent))
+    // resp.send({currentFilePath,metaUrl: import.meta.url,authorsJSONpath}) 
+})
+
+authorsRouter.get('/:authorID',(request,response) => {
+
+    const authors = JSON.parse(fs.readFileSync(authorsJSONpath))
+    const author = authors.find(s => s.id === request.params.authorID)
+    response.send(author)
+    
+})
+
+authorsRouter.put('/:id',(req,resp)=>{resp.send('hello put')})
+authorsRouter.delete('/:id',(req,resp)=>{resp.send('hello delete')})
+
 
 export default authorsRouter

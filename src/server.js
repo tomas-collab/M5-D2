2b5b043b4627b19b13513152627472bcb3c0ae9c
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-08-11 09:12:13 +0200

    all methods working properly

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ import listEndPoints from 'express-list-endpoints'
 import authorsRouter from './services/authors/index.js'
 const server = express()
 const port = 3001
+server.use(express.json())
+
 server.use('/authors', authorsRouter)
 console.table(listEndPoints(server))
 server.listen(port,()=>{
--- src/services/authors/index.js
+++ src/services/authors/index.js
@@ -2,6 +2,9 @@ import express from 'express'
 import { fileURLToPath } from 'url'
 import {dirname,join} from 'path'
 import fs from 'fs'
+import uniqid from 'uniqid'
+
+
 
 
 
@@ -10,25 +13,52 @@ const currentFilePath = fileURLToPath(import.meta.url)
 const currnetDirPath = dirname(currentFilePath)
 const authorsJSONpath = join(currnetDirPath , 'authors.json')
 
-
-authorsRouter.post('/',(req,resp)=>{resp.send('hello post')})
+                                 // post 
+authorsRouter.post('/',(request,response)=>{
+    console.log(request.body)
+    console.log(uniqid())
+    //step 1
+    const newAuthors = {...request.body, id:uniqid(), createdAt: new Date()}
+    //step 2
+    const authors = JSON.parse(fs.readFileSync(authorsJSONpath))
+    //step 3
+    authors.push(newAuthors)
+    //step 4
+    fs.writeFileSync(authorsJSONpath,JSON.stringify(authors))
+    response.status(201).send({id:newAuthors.id})
+})
+   
+                                //get1
 authorsRouter.get('/',(request,response)=>{
     const fileContent = fs.readFileSync(authorsJSONpath)
+   
    
     response.send(JSON.parse(fileContent))
     // resp.send({currentFilePath,metaUrl: import.meta.url,authorsJSONpath}) 
 })
-
+                               //get2
 authorsRouter.get('/:authorID',(request,response) => {
 
     const authors = JSON.parse(fs.readFileSync(authorsJSONpath))
+    
     const author = authors.find(s => s.id === request.params.authorID)
     response.send(author)
     
 })
-
-authorsRouter.put('/:id',(req,resp)=>{resp.send('hello put')})
-authorsRouter.delete('/:id',(req,resp)=>{resp.send('hello delete')})
+                               //----------------------------put------------------------------------|
+authorsRouter.put('/:authorID',(request,response)=>{
+    const authors = JSON.parse(fs.readFileSync(authorsJSONpath))
+    const remainingAuthors = authors.filter(author => author.id !== request.params.authorID)
+    const updateAuthors = {...request.body,id:request.params.authorID}
+    remainingAuthors.push(updateAuthors)
+    fs.writeFileSync(authorsJSONpath,JSON.stringify(remainingAuthors))
+    response.send(updateAuthors)})
+                              //delete
+authorsRouter.delete('/:authorID',(request,response)=>{
+    const authors = JSON.parse(fs.readFileSync(authorsJSONpath))
+    const remainingAuthors = authors.filter(author => author.id !== request.params.authorID)
+    fs.writeFileSync(authorsJSONpath,JSON.stringify(remainingAuthors))
+    response.status(204).send()})
 
 
 export default authorsRouter

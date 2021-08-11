e62008fc913be2747f862c5a0f415b157d2e33c5
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-08-11 16:33:12 +0200

    last check

--- src/server.js
+++ src/server.js
@@ -2,12 +2,24 @@ import express from 'express'
 import cors from 'cors'
 import listEndPoints from 'express-list-endpoints'
 import authorsRouter from './services/authors/index.js'
+import booksRouter from './services/book/index.js'
+
 const server = express()
 const port = 3000
+//middleware
+const loggerMiddleware = (req,res,next)=>{
+    console.log(`req method ${req.method} -- req URL ${req.url} -- ${new Date()}`)
+    next()
+}
+
+
+server.use(loggerMiddleware)
 server.use(cors())
 server.use(express.json())
 
+
 server.use('/authors', authorsRouter)
+server.use("/books",booksRouter)
 console.table(listEndPoints(server))
 server.listen(port,()=>{
     console.log('server listening on port ' + port)

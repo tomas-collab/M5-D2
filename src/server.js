9aa9a2e3def261fe8691ff8bbfead90324700406
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-08-17 14:05:13 +0200

    deleted book folder

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import listEndPoints from 'express-list-endpoints'
 import authorsRouter from './services/authors/index.js'
-import booksRouter from './services/book/index.js'
+
 
 const server = express()
 const port = 3000
@@ -19,7 +19,7 @@ server.use(express.json())
 
 
 server.use('/authors', authorsRouter)
-server.use("/books",booksRouter)
+
 console.table(listEndPoints(server))
 server.listen(port,()=>{
     console.log('server listening on port ' + port)
